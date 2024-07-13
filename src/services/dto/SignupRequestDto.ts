3b4e274497d5a86ae539fc2751938e34f52e124c
bereket-7 <bekhonelign@gmail.com>
2024-07-13 23:17:32 +0300

    dto signup

---
+++ src/services/dto/SignupRequestDto.ts
@@ -0,0 +1,10 @@
+export interface SignupRequestDto {
+    firstName: string;
+    lastName: string;
+    age: number;
+    sex: string;
+    address: string;
+    phone: string;
+    email: string;
+    password: string;
+}
